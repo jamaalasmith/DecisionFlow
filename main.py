2b3bd5d74bdc14e38c9e4a97f7b317bd3e73ff7b
jamaalasmith <jamaalsmith010@gmail.com>
2025-07-22 11:07:10 -0500

    Apply .gitignore cleanup

---
+++ main.py
@@ -0,0 +1,6 @@
+def greet(name):
+   return f"Welcome back, {name}, hope you're having a nice day!"
+
+# only run if this file is executed directly (Not imported)
+if __name__ == "__main__":
+   print(greet("Jamaal"))
